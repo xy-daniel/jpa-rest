5c17ea6c1390c4c4583780e04312295967c85b6d
p4m4p <myarctic@163.com>
2020-03-14 11:44:29 +0800

    REST自定义查询方法

--- src/main/java/org/javaboy/jparest/JpaRestApplication.java
+++ src/main/java/org/javaboy/jparest/JpaRestApplication.java
@@ -17,6 +17,40 @@ public class JpaRestApplication {
      * http://localhost:8080/books/6 ----> put ----> {"name":"朝花夕拾","author":"鲁迅"}
      * 删除
      * http://localhost:8080/books/6
+     *
+     * 自定义查询
+     * 查找所有可用自定义接口
+     * http://localhost:8080/books/search
+     * 根据书名查找书籍---->解决方法名称过长的问题---->方法名@RestResource(exported = true, path = "/findBookByName", rel = "findBookByName")---->http://localhost:8080/books/search/findBookByName?name=西游记
+     * http://localhost:8080/books/search/findBookByNameContaining?name=西游记
+     *
+     * 默认books---->修改默认---->@RepositoryRestResource(path = "/bs", collectionResourceRel = "bs", itemResourceRel="b")
+     * http://localhost:8080/bs/search/findBookByName?name=三
+     * http://localhost:8080/bs/search
+     * {
+     *   "_embedded": {
+     *     "bs": [    ----->collectionResourceRel
+     *       {
+     *         "name": "三国演义",
+     *         "author": "罗贯中",
+     *         "_links": {
+     *           "self": {
+     *             "href": "http://localhost:8080/bs/2"
+     *           },
+     *           "b": {    ----->itemResourceRel
+     *             "href": "http://localhost:8080/bs/2"
+     *           }
+     *         }
+     *       }
+     *     ]
+     *   },
+     *   "_links": {
+     *     "self": {
+     *       "href": "http://localhost:8080/bs/search/findBookByName?name=%E4%B8%89"
+     *     }
+     *   }
+     * }
+     *
      */
     public static void main(String[] args) {
         SpringApplication.run(JpaRestApplication.class, args);
--- src/main/java/org/javaboy/jparest/dao/BookDao.java
+++ src/main/java/org/javaboy/jparest/dao/BookDao.java
@@ -2,11 +2,22 @@ package org.javaboy.jparest.dao;
 
 import org.javaboy.jparest.bean.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 /**
  * @author daniel
  * @version 1.0.0
  * @date 2020/3/14 1:34
  */
+@RepositoryRestResource(path = "bs", collectionResourceRel = "bs", itemResourceRel="b")
 public interface BookDao extends JpaRepository<Book, Integer> {
+
+    //自定义查询
+    @RestResource(exported = true, path = "findBookByName", rel = "findBookByName")
+    List<Book> findBookByNameContaining(@Param("name") String name);
+
 }
