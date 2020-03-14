c53c0d3df05edb6edb5aa67183b165a0eb43faed
p4m4p <myarctic@163.com>
2020-03-14 11:23:34 +0800

    REST依赖自带的增删改查

--- src/main/java/org/javaboy/jparest/JpaRestApplication.java
+++ src/main/java/org/javaboy/jparest/JpaRestApplication.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaRestApplication {
 
+    /**
+     * 查询
+     * http://localhost:8080/books
+     * http://localhost:8080/books?page=1&size=2&sort=id,desc
+     * http://localhost:8080/books/1
+     * 添加
+     * http://localhost:8080/books ----> post ----> {"name":"野草","author":"鲁迅"}
+     * 更新
+     * http://localhost:8080/books/6 ----> put ----> {"name":"朝花夕拾","author":"鲁迅"}
+     * 删除
+     * http://localhost:8080/books/6
+     */
     public static void main(String[] args) {
         SpringApplication.run(JpaRestApplication.class, args);
     }
+
 
 }
